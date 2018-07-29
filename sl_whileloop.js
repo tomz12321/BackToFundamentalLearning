76e8a25882aeddda52f24fdd2efe2368cb319ed9
tomz123 <tomz12321@gmail.com>
2018-07-29 22:21:02 +1000

    Practice of while loop and do while loop
    29/07/2018 Practice of while loop and do while loop

--- sl_whileloop.js
+++ sl_whileloop.js
@@ -1,6 +1,7 @@
-//while (condition){
-	//code block
-//}
+/* while (condition){
+ * 	 //code block
+ * }
+ */
 
 //The while loop repeats through a block of code, as long as a specified condition is true.
 var x = 1;
@@ -8,3 +9,22 @@ var x = 1;
 		document.write(x + "<br/>");
 		x = x + 1;
 	}
+
+//pitfalls
+//If you forget to increase the variable used in the condition, the loop will never end.
+
+/* The do while loop
+ *
+ * do
+ * {
+ *		//code block
+ * }
+ * while (condition)
+ */
+
+var i = 20;
+do {
+	document.write(i+"<br/>");
+	i++
+}
+while (i <= 25);
