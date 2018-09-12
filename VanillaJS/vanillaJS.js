a580a7e9d29e2843aebd6c943615a5506db6ee03
tomz123 <tomz12321@gmail.com>
2018-09-13 09:32:28 +1000

    Upload note for vanilla js
    Upload note for vanilla js
    
    Source: http://vanilla-js.com/

---
+++ VanillaJS/vanillaJS.js
@@ -0,0 +1,41 @@
+/** = Vanilla JS  is a fast, lightweight, cross-platform framework =
+ * for building incredible, powerful JavaScript applications.
+ */
+ 
+Introduction
+
+/* The VanillaJS team maintains every byte of code in the framework and works hard each day to make
+ * sure it is small and intuitive. Who's using Vanilla JS? Glad you asked! Here are a few:
+ */
+
+ Facebook Google YouTube Yahoo Wikipedia Windows Live Twitter Amazon LinkedIn MSN
+ eBay Microsoft Tumblr Apple Pinterest PayPal Reddit Netfix Stack Overflow
+
+ In fact, Vanilla JS is already used on more websites than jQuery, Prototype JS, MooTools, YUI, and
+ Google Web Toolkit - combined.
+
+ Download
+
+ Testimonials
+
+ Getting Started
+
+ <script src="path/to/vanilla.js"></script>
+
+ Spead Comparison
+
+ VanillaJS / document.getElementById('test-table');
+ Dojo / dojo.byId('test-table');
+ PrototypeJS / $('test-table')
+ Ext JS / delete Ext.elCache['test-table']; Ext.get('test-table');
+ jQuery / $jq('#test-table');
+ YUI / YAHOO.util.Dom.get('test-table');
+ MooTools / document.id('test-table')
+
+ Code Examples
+
+ Fade an element out and then remove it
+
+ Make an AJAX call
+
+ Futher Reading
