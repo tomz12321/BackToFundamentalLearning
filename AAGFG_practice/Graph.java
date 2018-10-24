4089ae7e22441d9227297ea3e3dd3ca57252488a
tomz123 <tomz12321@gmail.com>
2018-10-24 15:55:21 +1100

    Fix comment of implement Bellman–Ford Algorithm
    Practice of implement Bellman–Ford Algorithm
    - Fix comment

--- AAGFG_practice/Graph.java
+++ AAGFG_practice/Graph.java
@@ -15,21 +15,21 @@
  * Practiced by Jyh-woei Yang (Tom) on 2018/10/24
  */
 
-Input: Graph and a source vertex src
-Output: Shortest distance to all vertices from src. If there is a negative weight cycle, then shortest distances are not calculated, negative weight cycle is reported.
+// Input: Graph and a source vertex src
+// Output: Shortest distance to all vertices from src. If there is a negative weight cycle, then shortest distances are not calculated, negative weight cycle is reported.
 
-1) This step initializes distances from source to all vertices as infinite and distance to source itself as 0. Create an array dist[] of size |V| with all values as infinite except dist[src] where src is source vertex.
+// 1) This step initializes distances from source to all vertices as infinite and distance to source itself as 0. Create an array dist[] of size |V| with all values as infinite except dist[src] where src is source vertex.
 
 
 
-2) This step calculates shortest distances. Do following |V|-1 times where |V| is the number of vertices in given graph.
-…..a) Do following for each edge u-v
-………………If dist[v] > dist[u] + weight of edge uv, then update dist[v]
-………………….dist[v] = dist[u] + weight of edge uv
+// 2) This step calculates shortest distances. Do following |V|-1 times where |V| is the number of vertices in given graph.
+// …..a) Do following for each edge u-v
+// ………………If dist[v] > dist[u] + weight of edge uv, then update dist[v]
+// ………………….dist[v] = dist[u] + weight of edge uv
 
-3) This step reports if there is a negative weight cycle in graph. Do following for each edge u-v
-……If dist[v] > dist[u] + weight of edge uv, then “Graph contains negative weight cycle”
-The idea of step 3 is, step 2 guarantees shortest distances if graph doesn’t contain negative weight cycle. If we iterate through all edges one more time and get a shorter path for any vertex, then there is a negative weight cycle
+// 3) This step reports if there is a negative weight cycle in graph. Do following for each edge u-v
+// ……If dist[v] > dist[u] + weight of edge uv, then “Graph contains negative weight cycle”
+// The idea of step 3 is, step 2 guarantees shortest distances if graph doesn’t contain negative weight cycle. If we iterate through all edges one more time and get a shorter path for any vertex, then there is a negative weight cycle
 
 // Examples:
 // Output: 
