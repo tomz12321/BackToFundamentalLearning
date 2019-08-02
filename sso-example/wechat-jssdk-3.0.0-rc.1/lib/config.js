5d44d68d942135545deec15232eb56a95db76742
tom.yang <tom.yang@pimwa.com>
2019-08-02 17:03:03 +1000

    Upload practice of OAuth
    Upload practice of OAuth

---
+++ sso-example/wechat-jssdk-3.0.0-rc.1/lib/config.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const _ = require('lodash');
+
+const wechatConfig = {
+  "wechatRedirectHost": "http://127.0.0.1", //redirect host in oauth redirect
+  "wechatRedirectUrl": "", //full redirect url in oauth redirect, e.g http://127.0.0.1/wechat/oauth-callback
+  "wechatToken": "", //your wechat token set in your https://mp.weixin.qq.com/advanced/advanced?action=dev&t=advanced/dev&token=1244756112&lang=zh_CN
+  "appId": "", //your wechat appId
+  "appSecret": "", // your wechat appSecret
+  "ticketUrl": "https://api.weixin.qq.com/cgi-bin/ticket/getticket",
+  "accessTokenUrl": "https://api.weixin.qq.com/cgi-bin/token",
+  "oAuthUrl": "https://open.weixin.qq.com/connect/oauth2/authorize",
+  "apiUrl": "https://api.weixin.qq.com",
+  "oAuthState": "", //state in oauth callback query
+};
+
+/**
+ * Check if user passed necessary configuration
+ * @param {object} options user custom wechat config
+ */
+exports.checkPassedConfiguration = (options) => {
+  if(_.isEmpty(options)) {
+    throw new Error('you need to pass the wechat configuration');
+  }
+  if(!options.wechatToken) {
+    throw new Error('wechat token not found');
+  }
+  if(!options.appId) {
+    throw new Error('wechat appId not found');
+  }
+  if(!options.appSecret) {
+    throw new Error('wechat appSecret not found');
+  }
+  return true;
+};
+
+/**
+ * get default wechat configuration
+ * @return {object} wechatConfig
+ */
+exports.getDefaultConfiguration = () => wechatConfig;
